cd33d70f6cd9d8f3a2f6f7831711c8230d21649d
Protesilaos Stavrou <info@protesilaos.com>
2025-04-29 13:02:28 +0300

    Bump version to trigger GNU ELPA rebuild
    This is because I messed up with the last release where some changes
    where not part of the commit that was tagged as 0.6.0.

--- spacious-padding.el
+++ spacious-padding.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/spacious-padding
-;; Version: 0.6.0
+;; Version: 0.6.1
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience, focus, writing, presentation
 
