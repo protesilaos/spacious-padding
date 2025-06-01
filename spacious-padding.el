60d1a343d2e740920750cdfc389972b363e73411
Protesilaos Stavrou <info@protesilaos.com>
2025-06-01 07:19:41 +0300

    Update to spacious-padding version 0.7.0

--- spacious-padding.el
+++ spacious-padding.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/spacious-padding
-;; Version: 0.6.1
+;; Version: 0.7.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience, focus, writing, presentation
 
